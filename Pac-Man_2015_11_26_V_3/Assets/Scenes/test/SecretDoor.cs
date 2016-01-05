ead98c8c9773737ea89734c45bb182fd194eb808
PFBCmaster <Fredrik@famnen.se>
2016-01-05 12:55:36 +0100

    Hemligdörr script
    Denna funkar i mitt spel hoppas att den funkar för alla andra. Om man
    kliver på denna ska en vägg med taggen secretDoor att stängas av så att
    man kan gå där.

---
+++ Pac-Man_2015_11_26_V_3/Assets/Scenes/test/SecretDoor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecretDoor : MonoBehaviour {
+
+	GameObject secretDoor;
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "Pacman") 
+		{
+			GameObject.FindGameObjectWithTag ("secretDoor");
+			secretDoor.SetActive(false);
+		}
+	}
+}
